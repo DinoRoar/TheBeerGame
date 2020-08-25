4ce962b7053955b50e3b8af13d5b99d8fc78bd97
John Nicholas <john.nicholas@dinoroar.io>
2020-08-25 19:50:33 +0100

    login failed

---
+++ TheBeerGame.GameEngine.Spec/RegisterLoginSpec/Login_with_bad_password_will.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Serilog;
+using Serilog.Events;
+using TheBeerGame.GameEngine.LoginMessages;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TheBeerGame.GameEngine.Spec.RegisterLoginSpec
+{
+    public class Login_with_bad_password_will : AggregateSpecification<Player, Login>
+    {
+        public Login_with_bad_password_will(ITestOutputHelper helper)
+        {
+            new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.TestOutput(helper, LogEventLevel.Verbose)
+                .CreateLogger();
+        }
+
+        public override IEnumerable<Event> Given() =>
+            new List<Event>()
+            {
+                new PlayerCreated(
+                    new Login("playerName", new Password(new MockPasswordHasher(), "password")),
+                    "password")
+            };
+
+        public override Login When()
+        {
+            return new Login("playerName", new Password(new MockPasswordHasher(), "wrong"));
+        }
+
+        public override Player CreateAggregateRoot()
+        {
+            return new Player();
+        }
+
+        [Fact]
+        public void Fail()
+        {
+            var events = Whenxecute();
+            Assert.Single(events);
+            var e = events.First();
+            Assert.IsType<LoginFailed>(e);
+        }
+    }
+}
